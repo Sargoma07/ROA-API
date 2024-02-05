6f6927490457b9ba78f922fdcd24bc341b8cb905
Sargoma <sargoma07@gmail.com>
2024-02-05 19:06:56 +0300

    Init proj

---
+++ ROA.Rest.API/Program.cs
@@ -0,0 +1,41 @@
+namespace ROA.Rest.API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = CreateBuilder(args);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+
+    private static WebApplicationBuilder CreateBuilder(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddControllers();
+        return builder;
+    }
+}
